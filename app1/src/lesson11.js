df6706cefbea6f1e5bf38569120111d7fcb29218
theeasylearn <theeasylearn@gmail.com>
2023-12-30 20:28:28 +0530

    index

---
+++ app1/src/lesson11.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import ScoreBoard from './ScoreBoard';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ScoreBoard />)
