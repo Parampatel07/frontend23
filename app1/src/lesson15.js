bc3fb8e569088306ff0a55aa58327ac9711722c7
theeasylearn <theeasylearn@gmail.com>
2024-01-08 20:04:39 +0530

    index

---
+++ app1/src/lesson15.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Agreement from './Agreement';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Agreement />)
